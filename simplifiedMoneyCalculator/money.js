c45652da325b39ee89de79d1fa6f62661aa4d060
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-07 15:10:07 +0300

    addes simplifiedmoneycalculator

---
+++ simplifiedMoneyCalculator/money.js
@@ -0,0 +1,10 @@
+function EstimatedTransactionFee(){
+    let money(number);
+    if(number === 1.5% || number === 10){
+        console.log(Math.min)
+    }
+    elseif(number === 1.5% || number ====70){
+        console.log(Math.max);
+    }
+}
+console.log(amount to send + transaction fee);
