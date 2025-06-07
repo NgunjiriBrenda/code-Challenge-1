6065ffb3ef5c208ea0b2ee289f95f98cd61a6856
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-07 14:37:59 +0300

    adds bodaFareCalculator

---
+++ bodaFareCalculator/fare.js
@@ -0,0 +1,12 @@
+function calculateBodaFare(){
+    let user = prompt("Enter distance of trips in kilometers");
+     distance = number(Kilometers);
+     const BaseFare = 50;
+     const per Kilometer = 15;
+     const totalFare = baseFare + (distanceInKm * chargePerKm);
+
+
+     console.log(`Distance: ${distance} in Km`);
+     console.log(`Fare: ${fare in shillings}`);
+
+};
